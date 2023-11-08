40aefc758e641804e5fdc615d51f7dbad53afe5f
Sushant Pattanaik <112720762+sushantpattanaik@users.noreply.github.com>
2023-11-09 01:53:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,10 @@ streamlit.dataframe(my_fruit_list)
 
 
 
+#New Section to display fruityvice api response
+import requests
+fruityvice_response requests.get("https://fruityvice.com/api/fruit/watermelon") 
+streamlit.text(fruityvice_response)
+
+
 
