d7e5abfd1f226a8bf1bf32b99488e5cc0bfb06eb
Sushant Pattanaik <112720762+sushantpattanaik@users.noreply.github.com>
2023-11-09 01:56:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+#New Section to display fruityvice api response 
+streamlit.header('Fruityvice Fruit Advice!')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
